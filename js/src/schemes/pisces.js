723a784f283c19b8b908b228424ec9a0feadde74
peterzhang <peterzhang@raventech.com>
2017-08-17 18:26:58 +0800

    Site updated: 2017-08-17 18:27:07

--- js/src/schemes/pisces.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var i=function(){return n.height()+10};var a=function(n){return r.css({"margin-top":n})};var e=window.matchMedia("(min-width: 991px)");a(i()).show();e.addListener(function(n){if(n.matches){a(i())}})});
